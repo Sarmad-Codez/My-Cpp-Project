dc5f49420e2de360c96a6821d8c9ebdc3467b835
Sarmad Ali <sarmad.ali2307@gmail.com>
2026-06-07 14:24:28 +0500

    Add files via upload

---
+++ First.cpp
@@ -0,0 +1,11 @@
+#include <iostream>   // Yeh input/output ki library hai
+using namespace std;  // Is se humein baar baar std::cout nahi likhna parta
+
+int main() {          // Yahan se program ka dil shuru hota hai
+    
+    // Screen par dikhane ke liye
+    cout << "Hello World!" << endl; 
+    cout << "Sarmad is now a C++ Pro in the making." << endl;
+
+    return 0;         // Program sahi se khatam hua
+}
