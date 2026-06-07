66e710de5a9190e2dc1d60c19b81f80b060c9874
Sarmad Ali <sarmad.ali2307@gmail.com>
2026-06-07 14:32:32 +0500

    Add files via upload

---
+++ Sixth.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+// 1. Function ki Definition (Hum ne machine banayi)
+void greet() {
+    cout << "Sarmad, mehnat rang layegi!" << endl;
+    cout << "Programming mushkil nahi hai." << endl;
+}
+
+int main() {
+    // 2. Function Calling (Hum ne machine ko chalaya)
+    greet(); 
+    
+    // Aap isay jitni dafa chahein bula sakte hain
+    greet(); 
+
+    return 0;
+}
