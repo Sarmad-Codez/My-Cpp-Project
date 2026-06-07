dfd18216b5b50337d6e0f2343751a06dccadad2c
Sarmad Ali <sarmad.ali2307@gmail.com>
2026-06-07 14:31:32 +0500

    Add files via upload

---
+++ Fourth.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // Loop shuru ho raha hai
+    for (int i = 1; i <= 10; i++) {
+        cout << "Number: " << i << endl;
+    }
+
+    cout << "Loop khatam ho gaya!" << endl;
+
+    return 0;
+}
